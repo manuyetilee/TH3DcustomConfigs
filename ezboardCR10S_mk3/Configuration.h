a7ecde73b891cf4c4b329958510f95320bb5192c
Conejo Ramirez <manuyetilee@me.com>
2022-04-08 17:03:55 -0400

    Modification for custom qty of pixels to handle in addressable leds

--- ezboardCR10S_mk3/Configuration.h
+++ ezboardCR10S_mk3/Configuration.h
@@ -843,7 +843,7 @@
       #ifndef NEOPIXEL_PIN
         #define NEOPIXEL_PIN    PA8    // LED driving pin
       #endif
-      #define NEOPIXEL_PIXELS 15       // Number of LEDs in the strip. (Longest strip when NEOPIXEL2_SEPARATE is disabled.)
+      #define NEOPIXEL_PIXELS 20       // Number of LEDs in the strip. (Longest strip when NEOPIXEL2_SEPARATE is disabled.)
       #define NEOPIXEL_IS_SEQUENTIAL   // Sequential display for temperature change - LED by LED. Disable to change all LEDs at once.
       #define NEOPIXEL_BRIGHTNESS 127  // Initial brightness (0-255)
       #define NEOPIXEL_STARTUP_TEST  // Cycle through colors at startup
